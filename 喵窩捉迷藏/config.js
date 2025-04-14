b262841c50b97d95d22cea538c3825e97bdccdf9
MiofaOwO <abc2200052@gmail.com>
2025-04-14 18:47:08 +0800

    Add files via upload

---
+++ 喵窩捉迷藏/config.js
@@ -0,0 +1,35 @@
+// 設定圖片分類與對應的檔案名稱
+const IMAGE_LIBRARY = {
+  official: [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+    "9.png",
+    "10.png",
+    "11.png",
+    "12.png",
+    "13.png",
+    "14.png"
+  ],
+  all: [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png",
+    "5.png",
+    "6.png",
+    "7.png",
+    "8.png",
+    "9.png",
+    "10.png",
+    "11.png",
+    "12.png",
+    "13.png",
+    "14.png"
+  ]
+};
